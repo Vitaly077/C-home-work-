e4bb8869f221e0d3513a739ae449325026102935
Vitaly <kentanohin478@gmail.com>
2022-07-10 19:01:00 +0300

    Семинар №3

---
+++ Задача 23/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите любую цифру");
+int value = Convert.ToInt32(Console.ReadLine());
+for (int power = 0; power <= 32; power++)
+Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power):N0} (0x{(long)Math.Pow(value, power):X})");
